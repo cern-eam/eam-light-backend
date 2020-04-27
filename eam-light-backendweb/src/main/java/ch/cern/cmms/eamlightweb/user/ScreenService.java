f48c7eb38d80673dd87056f8e534b8b53fc95a53
Ricardo Almeida <ricardo.almeida@cern.ch>
2020-04-27 15:28:54 +0200

    [HOTFIX] Read grid screens through JPAGrids

--- eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/user/ScreenService.java
+++ eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/user/ScreenService.java
@@ -42,6 +42,7 @@ public class ScreenService {
         Map<String, String> functions = getFunctions(context);
 
         GridRequest gridRequestLayout = new GridRequest("BSMESP_HDR", 2000);
+        gridRequestLayout.setUseNative(false);
         gridRequestLayout.addFilter("usergroup", userGroup, "=", JOINER.AND);
 
         // Record view filter
