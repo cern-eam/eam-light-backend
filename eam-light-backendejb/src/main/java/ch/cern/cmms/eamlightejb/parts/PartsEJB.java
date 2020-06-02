4a6077a04d01541a3429e3859f9a4b2233d3f0e4
Josephine Petersen <josephine.petersen@cern.ch>
2020-06-02 15:27:12 +0200

    [EAM-444] Changed spaces to tabs

--- eam-light-backendejb/src/main/java/ch/cern/cmms/eamlightejb/parts/PartsEJB.java
+++ eam-light-backendejb/src/main/java/ch/cern/cmms/eamlightejb/parts/PartsEJB.java
@@ -25,107 +25,66 @@ import ch.cern.cmms.eamlightejb.tools.LoggingService;
 @LocalBean
 public class PartsEJB {
 
-    @Inject
-    private InforClient inforClient;
+	@Inject
+	private InforClient inforClient;
 
-    @Inject
-    private LoggingService logger;
-
-
-    //
-    // Used to generate new code for parts
-    //
-    private static String GET_NEXT_PART_CODE =
-        "select lpad(NVL(max(substr(par_code, - 6)),0) + 1, 6, '0') as next_obj_code from r5parts "
-            + " where par_code like CONCAT(:prefixcode,'%') and regexp_like(par_code, CONCAT(:prefixcode,'[[:digit:]]{6}$')) ";
-
-    public Optional<String> getNextAvailablePartCode(String prefixcode) {
-        if (prefixcode == null || prefixcode.isEmpty()) {
-            return Optional.ofNullable(null);
-        }
-
-        prefixcode = prefixcode.replaceAll("[^a-zA-Z_-]", "");
-        if (prefixcode.isEmpty()) {
-            return Optional.ofNullable(null);
-        }
-
-        String newPartCode = null;
-        try {
-            newPartCode = (String) inforClient.getTools().getEntityManager().createNativeQuery(GET_NEXT_PART_CODE)
-                .setParameter("prefixcode", prefixcode)
-                .getSingleResult();
-        } catch (NoResultException exception) {
-            // nothing to do in this case
-            logger.log(Level.ERROR, exception.getMessage());
-        } catch (Exception exception) {
-            logger.log(Level.ERROR, exception.getMessage());
-        }
-
-        if (newPartCode == null) {
-            return Optional.ofNullable(null);
-        }
-
-        return Optional.ofNullable(prefixcode + newPartCode);
-    }
+	@Inject
+	private LoggingService logger;
 
 
-    public Optional<String> getNextAvailablePartCodeGrid(String prefixCode, InforContext context) {
-        GridRequest gridRequest = new GridRequest("SSPART", 1);
-        gridRequest.addFilter("partCode", prefixCode, "BEGINS");
-        gridRequest.sortBy("partCode", "DESC");
+	public Optional<String> getNextAvailablePartCodeGrid(String prefixCode, InforContext context) {
+		GridRequest gridRequest = new GridRequest("SSPART", 1);
+		gridRequest.addFilter("partCode", prefixCode, "BEGINS");
+		gridRequest.sortBy("partCode", "DESC");
 
-        String newPartCode = null;
+		String newPartCode = null;
 
-        try {
-            GridRequestResult grd =
-                inforClient.getGridsService().executeQuery(context,
-                    gridRequest);
+		try {
+			GridRequestResult grd =
+				inforClient.getGridsService().executeQuery(context,
+					gridRequest);
 
-            String entry = inforClient.getTools().getGridTools().extractSingleResult(grd,
-                "partCode");
+			String entry = inforClient.getTools().getGridTools().extractSingleResult(grd,
+				"partCode");
 
-            boolean testForLetters = false;
+			boolean testForLetters = false;
 
-            String whatsLeft = entry.substring(prefixCode.length());
-            for (int i = 0; i < whatsLeft.length(); i++) {
-                if (!Character.isDigit(whatsLeft.charAt(i))) {
-                    for (int j = entry.length() - 1; j >= 0; j--) {
-                        if (!Character.isDigit(entry.charAt(j))) {
-                            testForLetters = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (!testForLetters) {
+			String whatsLeft = entry.substring(prefixCode.length());
+			for (int i = 0; i < whatsLeft.length(); i++) {
+				if (!Character.isDigit(whatsLeft.charAt(i))) {
+					for (int j = entry.length() - 1; j >= 0; j--) {
+						if (!Character.isDigit(entry.charAt(j))) {
+							testForLetters = true;
+							break;
+						}
+					}
+				}
+			}
+			if (!testForLetters) {
 
-                if (!Character.isDigit(entry.charAt(entry.length() - 1))) {
-                    newPartCode = prefixCode + "000001";
-                } else {
-                    for (int i = prefixCode.length() - 1; i >= 0; i--) {
-                        if (!Character.isDigit(entry.charAt(i))) {
-                            Integer newCode = Integer.parseInt(entry.substring(i + 1)) + 1;
-                            newPartCode = entry.replace(entry.substring(i + 1), newCode.toString());
-                            break;
-                        }
-                    }
+				if (!Character.isDigit(entry.charAt(entry.length() - 1))) {
+					newPartCode = prefixCode + "000001";
+				} else {
+					for (int i = prefixCode.length() - 1; i >= 0; i--) {
+						if (!Character.isDigit(entry.charAt(i))) {
+							Integer newCode = Integer.parseInt(entry.substring(i + 1)) + 1;
+							newPartCode = entry.replace(entry.substring(i + 1), newCode.toString());
+							break;
+						}
+					}
 
-                    return Optional.ofNullable(newPartCode);
-                }
-            }
+					return Optional.ofNullable(newPartCode);
+				}
+			}
 
-        } catch (NoResultException exception) {
-            logger.log(Level.ERROR, exception.getMessage());
-        } catch (Exception exception) {
-            logger.log(Level.ERROR, exception.getMessage());
-        }
+		} catch (NoResultException exception) {
+			logger.log(Level.ERROR, exception.getMessage());
+		} catch (Exception exception) {
+			logger.log(Level.ERROR, exception.getMessage());
+		}
 
-        if (newPartCode == null) {
-            return Optional.ofNullable(null);
-        }
-
-        return Optional.ofNullable(newPartCode);
-    }
+		return Optional.ofNullable(newPartCode);
+	}
 
 
 }
