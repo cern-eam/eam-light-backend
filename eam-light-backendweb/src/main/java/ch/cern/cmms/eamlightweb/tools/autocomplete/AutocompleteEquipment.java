5d4c3c074abcd27585f61b4920ee89596aa02e8b
Ricardo Almeida <ricardo.almeida@cern.ch>
2022-04-21 19:00:58 +0200

    Fixed issue with equipment autocomplete grid filters

--- eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/tools/autocomplete/AutocompleteEquipment.java
+++ eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/tools/autocomplete/AutocompleteEquipment.java
@@ -1,6 +1,9 @@
 package ch.cern.cmms.eamlightweb.tools.autocomplete;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.interceptor.Interceptors;
@@ -13,6 +16,7 @@ import javax.ws.rs.QueryParam;
 
 import ch.cern.cmms.eamlightweb.tools.EAMLightController;
 import ch.cern.cmms.eamlightweb.tools.interceptors.RESTLoggingInterceptor;
+import ch.cern.eam.wshub.core.services.entities.Pair;
 import ch.cern.eam.wshub.core.services.grids.entities.GridRequest;
 import ch.cern.eam.wshub.core.services.grids.entities.GridRequestFilter;
 import ch.cern.eam.wshub.core.tools.InforException;
@@ -43,13 +47,24 @@ public class AutocompleteEquipment extends EAMLightController {
 	@Consumes("application/json")
 	public Response complete(@QueryParam("s") String code, @QueryParam("a") String alias, @QueryParam("n") String serialNo) {
 		GridRequest gridRequest = prepareGridRequest(GridRequest.GRIDTYPE.LOV);
-		gridRequest.addFilter("equipmentno", code.toUpperCase(), "BEGINS", GridRequestFilter.JOINER.OR);
-		if (alias != null) {
-			gridRequest.addFilter("alias", alias.toUpperCase(), "BEGINS", GridRequestFilter.JOINER.OR);
+
+		List<Pair> gridFilters = new ArrayList<>(Collections.singletonList(new Pair("equipmentcode", code)));
+		if (alias != null && alias.trim().length() > 0) {
+			gridFilters.add(new Pair("alias", alias));
 		}
-		if (serialNo != null) {
-			gridRequest.addFilter("serialnumber", serialNo.toUpperCase(), "BEGINS");
+		if (serialNo != null && serialNo.trim().length() > 0) {
+			gridFilters.add(new Pair("serialno", serialNo));
 		}
+
+		for (Pair column: gridFilters) {
+			boolean first = column.getCode().equals(gridFilters.get(0).getCode());
+			boolean last = column.getCode().equals(gridFilters.get(gridFilters.size() - 1).getCode());
+
+			gridRequest.addFilter(column.getCode(), column.getDesc().toUpperCase(), "BEGINS",
+					last ? GridRequestFilter.JOINER.AND : GridRequestFilter.JOINER.OR,
+					first, last);
+		}
+
 		return getPairListResponse(gridRequest, "equipmentcode", "equipmentdesc");
 	}
 
