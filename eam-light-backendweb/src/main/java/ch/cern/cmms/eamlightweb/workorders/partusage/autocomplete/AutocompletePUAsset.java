9904ddd5db958bbc5975e88b7764d7249bed2b99
António Paulo <antonio.paulo@cern.ch>
2022-07-14 17:56:32 +0200

    Update endpoint to allow part filtering
    Changed to query params to deal with empty string
    as a value for 'part'. With path params we would
    get a path such as '.../transaction/store//code',
    which would not exist as an endpoint.
    
    Also, removed unused imports.

--- eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/workorders/partusage/autocomplete/AutocompletePUAsset.java
+++ eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/workorders/partusage/autocomplete/AutocompletePUAsset.java
@@ -6,17 +6,16 @@ package ch.cern.cmms.eamlightweb.workorders.partusage.autocomplete;
 import java.util.Arrays;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
 import javax.interceptor.Interceptors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import ch.cern.cmms.eamlightejb.data.ApplicationData;
-import ch.cern.cmms.eamlightweb.tools.EAMLightController;
 import ch.cern.cmms.eamlightweb.tools.EAMLightController;
 import ch.cern.cmms.eamlightweb.tools.interceptors.RESTLoggingInterceptor;
 import ch.cern.eam.wshub.core.services.grids.entities.GridRequest;
@@ -29,17 +28,21 @@ import ch.cern.eam.wshub.core.tools.InforException;
 public class AutocompletePUAsset extends EAMLightController {
 
 	@GET
-	@Path("/partusage/asset/{issuereturn}/{store}/{code}")
+	@Path("/partusage/asset")
 	@Produces("application/json")
-	@Consumes("application/json")
-	public Response complete(@PathParam("issuereturn") String issuereturn, @PathParam("store") String store, @PathParam("code") String code) {
+	public Response complete(@QueryParam("transaction") String transaction, @QueryParam("store") String store,
+							 @QueryParam("part") String part, @QueryParam("code") String code) {
 
 		GridRequest gridRequest = new GridRequest("OSOBJA", GridRequest.GRIDTYPE.LIST, ApplicationData.AUTOCOMPLETE_RESULT_SIZE);
 		gridRequest.addFilter("equipmentno", code.toUpperCase(), "CONTAINS", GridRequestFilter.JOINER.AND);
 
-		if (issuereturn.startsWith("I")) // ISSUE
+		if (!part.isEmpty()) {
+			gridRequest.addFilter("part", part, "=", GridRequestFilter.JOINER.AND);
+		}
+
+		if (transaction.equals("ISSUE")) {
 			gridRequest.addFilter("store", store, "=");
-		else { // RETURN
+		} else if (transaction.equals("RETURN")) {
 			gridRequest.addFilter("store", "", "IS EMPTY");
 		}
 
