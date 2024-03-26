4d9eb5f942eeebb0fecf851288001ac213e0732a
António Paulo <antonio.paulo@cern.ch>
2024-03-26 11:14:35 +0000

    [EAM-2225] Add lot list endpoint (#102)
    * [EAM-2225] Add lot list endpoint
    
    Uses newly added CERN-specific grid request to user defined screen of lots.

--- eam-light-opensource/src/main/java/ch/cern/cmms/plugins/SharedPluginImpl.java
+++ eam-light-opensource/src/main/java/ch/cern/cmms/plugins/SharedPluginImpl.java
@@ -1,8 +1,27 @@
 package ch.cern.cmms.plugins;
 
+import ch.cern.eam.wshub.core.client.InforClient;
+import ch.cern.eam.wshub.core.client.InforContext;
+import ch.cern.eam.wshub.core.services.entities.Pair;
+import ch.cern.eam.wshub.core.tools.InforException;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 public class SharedPluginImpl implements SharedPlugin {
     @Override
     public String sayHello() {
         return "Hello from Open Source!";
+    }
+
+    @Override
+    public Map<String, Map<String, String>> getDatesPermissions(final InforClient inforClient, final String username) {
+        return new HashMap<>();
+    }
+
+    @Override
+    public List<Pair> getUdsLots(String partCode, InforClient inforClient, InforContext context, Map<String, String> applicationData) throws InforException {
+        return new LinkedList<>();
     }
 }
