a44ce39c9cfdb8d8a106775c583caecd752d3b4a
Lukasz Pater <Lukasz.Pater@cern.ch>
2024-11-27 22:29:46 +0100

    Add endpoints for Autocomplete / Select lookups

--- eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/application/ApplicationController.java
+++ eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/application/ApplicationController.java
@@ -17,10 +17,16 @@ import ch.cern.cmms.eamlightweb.user.UserService;
 import ch.cern.cmms.plugins.LDAPPlugin;
 import ch.cern.cmms.plugins.SharedPlugin;
 import ch.cern.eam.wshub.core.client.InforClient;
+import ch.cern.eam.wshub.core.services.administration.entities.GenericLov;
 import ch.cern.eam.wshub.core.services.administration.impl.ScreenLayoutServiceImpl;
+import ch.cern.eam.wshub.core.services.grids.entities.GridRequest;
 import ch.cern.eam.wshub.core.services.grids.impl.GridsServiceImpl;
 import ch.cern.eam.wshub.core.services.grids.impl.InforGrids;
 import ch.cern.eam.wshub.core.services.workorders.impl.ChecklistServiceImpl;
+import ch.cern.eam.wshub.core.tools.InforException;
+
+import java.util.List;
+import java.util.Map;
 
 @Path("/application")
 @ApplicationScoped
@@ -85,4 +91,32 @@ public class ApplicationController extends EAMLightController {
 		String version = applicationData.getVersion();
 		return ok(version);
 	}
+
+	@POST
+	@Path("/lov")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public Response getGenericLov(GenericLov genericLov) {
+		try {
+			final List<Map<String, String>> genericLov1 = inforClient.getScreenLayoutService().getGenericLov(authenticationTools.getInforContext(), genericLov);
+			return ok(genericLov1);
+		} catch (InforException e) {
+			return badRequest(e);
+		} catch(Exception e) {
+			return serverError(e);
+		}
+	}
+
+	@GET
+	@Path("/codelov/{code}")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public Response getCodeLov(@PathParam("code") String code) {
+		GridRequest gridRequest = new GridRequest("LVALLCODES", GridRequest.GRIDTYPE.LOV, ApplicationData.AUTOCOMPLETE_RESULT_SIZE);
+		gridRequest.setUserFunctionName("LVALLCODES");
+		gridRequest.setUseNative(true);
+		gridRequest.addParam("parameter.rtype", code);
+		return getPairListResponse(gridRequest, "code", "description");
+	}
+
 }
