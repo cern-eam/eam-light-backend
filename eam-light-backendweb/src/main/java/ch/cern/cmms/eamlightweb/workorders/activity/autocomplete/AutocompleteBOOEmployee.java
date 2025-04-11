2a831b0ae315a97f7efb81248011ec0c10569a88
Pablo Urquijo <pablo.urquijo.martinez@cern.ch>
2025-04-11 17:17:09 +0200

    [EAM-4375] Accept params BOO autocomplete employee

--- eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/workorders/activity/autocomplete/AutocompleteBOOEmployee.java
+++ eam-light-backendweb/src/main/java/ch/cern/cmms/eamlightweb/workorders/activity/autocomplete/AutocompleteBOOEmployee.java
@@ -1,46 +1,41 @@
 package ch.cern.cmms.eamlightweb.workorders.activity.autocomplete;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.interceptor.Interceptors;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
 import ch.cern.cmms.eamlightweb.tools.EAMLightController;
 import ch.cern.cmms.eamlightweb.tools.interceptors.RESTLoggingInterceptor;
 import ch.cern.eam.wshub.core.services.grids.entities.GridRequest;
 import ch.cern.eam.wshub.core.services.grids.entities.GridRequestFilter;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.interceptor.Interceptors;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
 @Path("/autocomplete")
 @ApplicationScoped
-@Interceptors({ RESTLoggingInterceptor.class })
+@Interceptors({RESTLoggingInterceptor.class})
 public class AutocompleteBOOEmployee extends EAMLightController {
 
-	@GET
-	@Path("/boo/employee/{code}")
-	@Produces("application/json")
-	@Consumes("application/json")
-	public Response complete(@PathParam("code") String code) {
-		GridRequest gridRequest = new GridRequest("146", "LVEMP", "151");
-		gridRequest.setGridType(GridRequest.GRIDTYPE.LOV);
-		gridRequest.addParam("param.date", null);
-		gridRequest.addParam("parameter.per_type", null);
-		gridRequest.addParam("param.trade", "KPL");
-		gridRequest.addParam("param.act", "5");
-		gridRequest.addParam("param.booplan", "true");
-		gridRequest.addParam("param.event", "555555");
-		gridRequest.addParam("param.octype", "N");
+    @GET
+    @Path("/boo/event/{event}/activity/{act}/trade/{trade}/octype/{octype}/employee/{code}")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response complete(@PathParam("event") String event, @PathParam("act") String act, @PathParam("trade") String trade, @PathParam("octype") String octype, @PathParam("code") String code) {
+        GridRequest gridRequest = new GridRequest("146", "LVEMP", "151");
+        gridRequest.setGridType(GridRequest.GRIDTYPE.LOV);
+        gridRequest.addParam("param.date", null);
+        gridRequest.addParam("parameter.per_type", null);
+        gridRequest.addParam("param.trade", trade);
+        gridRequest.addParam("param.act", act);
+        gridRequest.addParam("param.booplan", "true");
+        gridRequest.addParam("param.event", event);
+        gridRequest.addParam("param.octype", octype);
 
-		gridRequest.getGridRequestFilters().add(new GridRequestFilter("personcode", code.toUpperCase(), "BEGINS", GridRequestFilter.JOINER.OR ));
-		gridRequest.getGridRequestFilters().add(new GridRequestFilter("description", code.toUpperCase(), "CONTAINS" ));
+        gridRequest.getGridRequestFilters().add(new GridRequestFilter("personcode", code.toUpperCase(), "BEGINS", GridRequestFilter.JOINER.OR));
+        gridRequest.getGridRequestFilters().add(new GridRequestFilter("description", code.toUpperCase(), "CONTAINS"));
 
-		gridRequest.sortBy("description");
+        gridRequest.sortBy("description");
 
-		return getPairListResponse(gridRequest, "personcode", "description");
-	}
+        return getPairListResponse(gridRequest, "personcode", "description");
+    }
 
 }
